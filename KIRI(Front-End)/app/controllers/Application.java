d59bcde5425b547937fc6efc6e46abd1be65af90
Steven sutana <altheroz@yahoo.co.id>
2015-11-17 23:40:22 +0700

    Application
    i18n

--- KIRI(Front-End)/app/controllers/Application.java
+++ KIRI(Front-End)/app/controllers/Application.java
@@ -3,9 +3,14 @@ package controllers;
 import models.User;
 import play.*;
 import play.data.Form;
+import play.db.DB;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Application extends Controller {
 
@@ -13,11 +18,40 @@ public class Application extends Controller {
 
 
     public Result index() {
+        ctx().changeLang("en");
+        return ok(index.render());
+    }
+
+    //i18n
+    public Result id() {
+        ctx().changeLang("id");
         return ok(index.render());
     }
 
     public Result findRoute() {
         return ok(index.render());
+    }
+
+    public Result testdb() {
+        Connection connection = null;
+        StringBuilder output = new StringBuilder();
+        try {
+            connection = DB.getConnection();
+            // Look for angkot.web.id refreshes
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SHOW TABLES;");
+            while (result.next()) {
+                output.append(result.getString(1) + "/");
+            }
+            connection.close();
+            return ok(output.toString());
+        } catch (Exception e) {
+            return internalServerError(e.toString());
+        } finally {
+            if (connection != null) {
+//                      connection.close();
+            }
+        }
     }
 
 //    public Result submit()
--- KIRI(Front-End)/public/js/index.js
+++ KIRI(Front-End)/public/js/index.js
@@ -2,7 +2,7 @@
  * Created by Node8 on 1/11/15.
  */
 
-
+var map;
 var input = document.getElementById("startInput");
 var output = document.getElementById("finishInput");
 
@@ -57,7 +57,7 @@ var mapLayer = new ol.layer.Tile(
 var resultVectorSource = new ol.source.Vector();
 var inputVectorSource = new ol.source.Vector();
 
-var map = new ol.Map({
+map = new ol.Map({
     target: 'map',
     layers : [ mapLayer, new ol.layer.Vector({source: inputVectorSource}), new ol.layer.Vector({source: resultVectorSource}) ],
     view: new ol.View({
@@ -78,7 +78,7 @@ map.on('click', function(event) {
         })
         markers['start'].setStyle(new ol.style.Style({
             image: new ol.style.Icon({
-                src: '@routes.Assets.versioned("images/start.png")',
+                src: '/assets/images/start.png',
                 anchor: [1.0, 1.0]
             })
         }));
@@ -90,39 +90,46 @@ map.on('click', function(event) {
         })
         markers['finish'].setStyle(new ol.style.Style({
             image: new ol.style.Icon({
-                src: '../images/finish.png',
+                src: 'assets/images/finish.png',
                 anchor: [0.0, 1.0]
             })
         }));
         inputVectorSource.addFeature(markers['finish']);
         finishInput.value = latLngToString(ol.proj.transform(event.coordinate, 'EPSG:3857', 'EPSG:4326'));
     }
-    //if ($('#startInput').val() === '') {
-    //    markers['start'] = new ol.Feature({
-    //        geometry: new ol.geom.Point(event.coordinate)
-    //    })
-    //    markers['start'].setStyle(new ol.style.Style({
-    //        image: new ol.style.Icon({
-    //            src: 'images/start.png',
-    //            anchor: [1.0, 1.0]
-    //        })
-    //    }));
-    //    inputVectorSource.addFeature(markers['start']);
-    //    $('#startInput').val(latLngToString(ol.proj.transform(event.coordinate, 'EPSG:3857', 'EPSG:4326')));
-    //} else if ($('#finishInput').val() === '') {
-    //    markers['finish'] = new ol.Feature({
-    //        geometry: new ol.geom.Point(event.coordinate)
-    //    })
-    //    markers['finish'].setStyle(new ol.style.Style({
-    //        image: new ol.style.Icon({
-    //            src: 'images/finish.png',
-    //            anchor: [0.0, 1.0]
-    //        })
-    //    }));
-    //    inputVectorSource.addFeature(markers['finish']);
-    //    $('#finishInput').val(latLngToString(ol.proj.transform(event.coordinate, 'EPSG:3857', 'EPSG:4326')));
-    //}
 });
+
+
+
+function clearRoutingResultsOnMap() {
+    resultVectorSource.clear();
+    //updateRegion(region, false);
+}
+
+function clearRoutingResultsOnTable() {
+    //var tabs = document.get
+    $('.tabs').remove();
+    $('.tabs-content').remove();
+}
+
+function clearAlerts() {
+    $('.alert-box').remove();
+}
+
+function clearSecondaryAlerts() {
+    $('.alert-box.secondary').fadeOut();
+}
+
+function clearStartFinishMarker() {
+    if (markers['start'] != null) {
+        markers['start'] = null;
+    }
+    if (markers['finish'] != null) {
+        markers['finish'] = null;
+    }
+    inputVectorSource.clear();
+}
+
 
 /**
  * Convert lon/lat into text representation
@@ -150,7 +157,7 @@ var updateMap = function(zoom){
     map.getView().setZoom(zoom);
 };
 
-var handler = function () {
+var handlerRegion = function () {
     var regionValue = regionSelect.value;
     if(regionValue=="bdg")
     {
@@ -172,7 +179,22 @@ var handler = function () {
     }
 
 };
-regionSelect.addEventListener("change",handler,false);
+regionSelect.addEventListener("change",handlerRegion,false);
+
+var localeSelect = document.getElementById("localeselect");
+var handlerLocale = function(){
+    var localeValue = localeSelect.value;
+    if(localeValue=="en")
+    {
+        window.location.replace("http://localhost:9000");
+    }
+    else if(localeValue =="id")
+    {
+        window.location.replace("http://localhost:9000/id");
+
+    }
+};
+localeSelect.addEventListener("change",handlerLocale,false);
 
 /**
  * Swap the inputs
